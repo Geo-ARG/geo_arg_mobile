9333281d668843969e86b9c0b9297530785631ed
hacktiv8 <hacktiv8@hacktiv8-4.local>
2017-03-17 15:35:37 +0700

    Second Load

--- src/components/loading_screen/Loading2.js
+++ src/components/loading_screen/Loading2.js
@@ -1,15 +1,50 @@
-import React, { Component } from 'react'
-import {View, Text, StyleSheet, Button, TextInput} from 'react-native'
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+} from 'react-native';
+import mainBackground from '../img/comment.gif'
 
+var {width, height} = require('Dimensions').get('window');
 
-export default class Second extends Component {
-    render() {
-      return (
-          <View>
-            <Text>
-                Second
-            </Text>
-          </View>
-      )
-    }
+export default class Loading2 extends Component {
+
+  componentDidMount(){
+    setTimeout(function(){
+      this.props.navigator.push({page:"loading2"})
+    }, 2000);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+               <Image source={require("../images/load.gif")} style={styles.imgHome} />
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    imgHome:{
+      flex: 1,
+      width: null,
+      height: null,
+    },
+    buttonHome: {
+      height: 60,
+    },
+    todos: {
+      fontSize: 20,
+      textAlign: 'center',
+      marginTop: 10,
+
+    },
+    searchForm: {
+      marginTop: 20,
+    }
+});
