79d12cf968b08e6673b1f8678c6cfcc91234d47d
hacktiv8 <hacktiv8@hacktiv8-4.local>
2017-03-17 15:34:47 +0700

    First Loading

---
+++ src/components/loading_screen/Loading1.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+} from 'react-native';
+import mainBackground from '../img/comment.gif'
+
+var {width, height} = require('Dimensions').get('window');
+
+export default class Loading1 extends Component {
+
+  componentDidMount(){
+    setTimeout(function(){
+      this.props.navigator.push({page:"loading1"})
+    }, 2000);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+               <Image source={require("../images/loading.gif")} style={styles.imgHome} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    imgHome:{
+      flex: 1,
+      width: null,
+      height: null,
+    },
+    buttonHome: {
+      height: 60,
+    },
+    todos: {
+      fontSize: 20,
+      textAlign: 'center',
+      marginTop: 10,
+
+    },
+    searchForm: {
+      marginTop: 20,
+    }
+});
---
+++ src/components/loading_screen/Loading2.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import {View, Text, StyleSheet, Button, TextInput} from 'react-native'
+
+
+export default class Second extends Component {
+    render() {
+      return (
+          <View>
+            <Text>
+                Second
+            </Text>
+          </View>
+      )
+    }
+}
