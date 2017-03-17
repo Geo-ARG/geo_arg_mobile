396fe21482edba147476e77df4867d6e1a7bc9f6
hacktiv8 <hacktiv8@hacktiv8-4.local>
2017-03-17 14:54:13 +0700

    Test Login FB and Google+

--- index.ios.js
+++ index.ios.js
@@ -4,50 +4,46 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, {Component} from 'react';
+import {AppRegistry, StyleSheet, Text, View} from 'react-native';
+var Auth0Lock = require('react-native-lock');
+// var lock = new Auth0Lock({clientId: 'xZAFgD4PIqldvAzGrhaNZpWHswGIrC25', domain: 'user-login.auth0.com'});
+var lock = new Auth0Lock("xZAFgD4PIqldvAzGrhaNZpWHswGIrC25", "user-login.auth0.com", {
+    allowedConnections: [
+        "Username-Password-Authentication", "google-oauth2", "twitter", "facebook"
+    ],
+    rememberLastLogin: false,
+    socialButtonStyle: "big",
+    theme: {},
+    languageDictionary: {
+        "title": "Auth0"
+    },
+    language: "en"
+});
 
 export default class geo_arg_mobile extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        lock.show()
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5
+    }
 });
 
 AppRegistry.registerComponent('geo_arg_mobile', () => geo_arg_mobile);
