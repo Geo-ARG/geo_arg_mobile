c36d1bea56a387fbe2a89123a7280e81a7e81e1e
YONI SETIAWAN <yonisetiawan1990@gmail.com>
2017-03-20 17:03:51 +0700

    fix merge2

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,148 +1,112 @@
 import React, {Component} from 'react';
 import {
-    StyleSheet,
-    View,
-    Image,
-    Text,
-    BackAndroid,
-    Dimensions,
-    TouchableHighlight,
-    AsyncStorage
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  BackAndroid,
+  TouchableHighlight,
+  AsyncStorage
 } from 'react-native';
-import Home from './Home'
-import Carousel from 'react-native-looped-carousel'
+import Home from './'
+
 var Auth0Lock = require('react-native-lock');
 var lock = new Auth0Lock({
-    clientId: 'xZAFgD4PIqldvAzGrhaNZpWHswGIrC25',
-    domain: 'user-login.auth0.com',
-    allowedConnections: [
-        "Username-Password-Authentication", "google-oauth2", "facebook"
-    ],
-    rememberLastLogin: true,
-    socialButtonStyle: "big",
-    theme: {},
-    languageDictionary: {
-        "title": "ARG Login"
-    },
-    language: "en"
+  clientId: 'xZAFgD4PIqldvAzGrhaNZpWHswGIrC25',
+  domain: 'user-login.auth0.com',
+  allowedConnections: [
+    "Username-Password-Authentication", "google-oauth2", "facebook"
+  ],
+  rememberLastLogin: true,
+  socialButtonStyle: "big",
+  theme: {},
+  languageDictionary: {
+    "title": "ARG Login"
+  },
+  language: "en"
 });
 
-const { width, height } = Dimensions.get('window')
-
 export default class Login extends Component {
-    constructor() {
-        super()
-        this.state = {
-            username: '',
-            email: '',
-            size: { width, height }
-        }
-        this.saveData = this.saveData.bind(this)
-        this.loginForm = this.loginForm.bind(this)
+  constructor() {
+    super()
+    this.state = {
+      username: '',
+      email: ''
     }
+    this.saveData = this.saveData.bind(this)
+    this.loginForm = this.loginForm.bind(this)
+  }
 
-    _onLayoutDidChange = (e) => {
-      const layout = e.nativeEvent.layout;
-      this.setState({size: {width: layout.width, height: layout.height}})
-    }
-
-    componentWillMount(){
-      // AsyncStorage.getItem('dataUser', (err, result) => {
-      //     if (result) {
-      //         this.props.navigator.push({page: 'home'})
-      //     }
-      // });
-    }
-
-
-    saveData() {
-        if (this.state.username != "") {
-            fetch('http://geo-arg-server-dev.ap-southeast-1.elasticbeanstalk.com/auth/users', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({username: this.state.username, email: this.state.email})
-            }).then(res => res.json()).then(newDataTodos => {
-                AsyncStorage.setItem('dataUser', JSON.stringify(this.state))
-            })
-
-        }
+  componentWillMount(){
+    AsyncStorage.getItem('dataUser', (err, result) => {
+      if (result) {
+        this.props.navigator.push({page: 'home'})
       }
+    });
+  }
 
 
-    loginForm(){
-      lock.show({
-          closable: true
-      }, (err, profile, token) => {
-          if (err) {
-              console.log(err);
-              return;
-          }
-          this.setState({username: profile.nickname, email: profile.email || ""})
-            this.saveData()
-          this.props.navigator.push({page: 'home'});
-      });
-    }
-
-    render() {
-      BackAndroid.addEventListener('LockBack', function() {
-        return true;
+  saveData() {
+    if (this.state.username != "") {
+      fetch('http://geo-arg-server-dev.ap-southeast-1.elasticbeanstalk.com/auth/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({username: this.state.username, email: this.state.email})
+      }).then(res => res.json()).then(newDataTodos => {
+        AsyncStorage.setItem('dataUser', JSON.stringify(this.state))
       })
-        return (
-          <View style={styles.container} onLayout={this._onLayoutDidChange}>
-              <Carousel
-                 delay={5000}
-                 style={this.state.size}
-                 autoplay
-                 bullets={true}>
-               <Image style={{flex: 1, width:'100%', height:'100%'}}
-                 source={require('../assets/1.jpg')} />
-               <Image style={{flex: 1, width:'100%', height:'100%'}}
-                 source={require('../assets/2.jpg')}/>
-               <Image style={{flex: 1, width:'100%', height:'100%'}}
-                 source={require('../assets/3.jpg')}/>
-             </Carousel>
-             <Image
-                 source={require('../assets/logo.png')}
-                 style={{
-                   position:'absolute',
-                   flexDirection: 'row'
-                 }}
-             />
 
-            <View style={styles.buttonLogin}>
-              <TouchableHighlight
-                  style={styles.signInButton}
-                  underlayColor='#1E90FF'
-                  onPress={this.loginForm}>
-                  <Text>Goto Game</Text>
-              </TouchableHighlight>
-            </View>
-          </View>
-        )
     }
+  }
+
+
+  loginForm(){
+    lock.show({
+      closable: true
+    }, (err, profile, token) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      this.setState({username: profile.nickname, email: profile.email || ""})
+      this.saveData()
+      this.props.navigator.push({page: 'home'});
+    });
+  }
+
+  render() {
+    BackAndroid.addEventListener('LockBack', function() {
+      return true;
+    })
+    return (
+      <View style={styles.container}>
+
+      <TouchableHighlight
+      style={styles.signInButton}
+      underlayColor='#949494'
+      onPress={this.loginForm}>
+      <Text>Goto Game</Text>
+      </TouchableHighlight>
+      <Image source={require("../assets/load.gif")} style={styles.imgHome} />
+      </View>
+    )
+  }
 
 }
 
 var styles = StyleSheet.create({
   container: {
+    flex: 1,
+    flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'center'
-  },
-  buttonLogin:{
-    position: 'absolute',
-       width: width,
-       bottom: width/3,
-       flexDirection: 'row',
-       justifyContent: 'center',
-       alignItems: 'center',
+    backgroundColor: '#15204C',
   },
   signInButton: {
     height: 50,
-    width: width/2,
     alignSelf: 'stretch',
-    backgroundColor: 'white',
+    backgroundColor: '#D9DADF',
     margin: 10,
     borderRadius: 5,
     justifyContent: 'center',
