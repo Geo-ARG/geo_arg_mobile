687e02191eab9a45afda12a2defcd76f8be9d93b
Fadly Kayo <fadly.kayo@googlemail.com>
2017-03-21 22:32:43 +0700

    Edit EventMap

--- src/components/EventMap.js
+++ src/components/EventMap.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { View, Text, StyleSheet, Dimensions } from 'react-native'
+import { View, Text, StyleSheet, Dimensions, Image } from 'react-native'
 import MapView from 'react-native-maps'
-import { fetchEvents } from '../actions'
+import { fetchEvents, clearEvents } from '../actions'
 import { Container, Header, Left, Button, Title, Content, Footer } from 'native-base';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 const {height, width} = Dimensions.get('window');
@@ -13,6 +13,10 @@ const styles = {
     height: height * 0.87,
     width: width
   },
+  loading: {
+    height: height * 0.9,
+    width: width
+  }
 }
 
 class EventMap extends React.Component {
@@ -37,6 +41,10 @@ class EventMap extends React.Component {
     this.props.fetchEvents()
   }
 
+  componentWillUnmount () {
+    this.props.clearEvents()
+  }
+
   render(){
     return(
       <Container style={{backgroundColor: '#F5F5F5'}}>
@@ -52,23 +60,28 @@ class EventMap extends React.Component {
           </Left>
         </Header>
         <Content style={{height: height * 0.9}}>
-          <MapView
-            style={styles.map}
-            region={this.state.region}
-            onRegionChange={this.onRegionChange}
-          >
-          {this.props.events.map((marker, index) => {
-            let coordinates = marker.geolocation.coordinates
-            return (
-              <MapView.Marker
-                key={index}
-                coordinate={{latitude: coordinates[0], longitude: coordinates[1]+0.002}}
-                title={marker.title}
-                image={require('../assets/pokeball.png')}
-                description={`${marker.place}, ${marker.description}`}/>
+          {this.props.events.length < 1
+            ?
+            <Image style={styles.loading} source={require('../assets/loading.gif')} />
+            :
+            <MapView
+              style={styles.map}
+              region={this.state.region}
+              onRegionChange={this.onRegionChange}
+            >
+            {this.props.events.map((marker, index) => {
+              let coordinates = marker.geolocation.coordinates
+              return (
+                <MapView.Marker
+                  key={index}
+                  coordinate={{latitude: coordinates[0], longitude: coordinates[1]+0.002}}
+                  title={marker.title}
+                  image={require('../assets/pokeball.png')}
+                  description={`${marker.place}, ${marker.description}`}/>
+              )}
             )}
-          )}
-          </MapView>
+            </MapView>
+          }
         </Content>
       </Container>
     )
@@ -82,7 +95,7 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = dispatch => {
-  return bindActionCreators({ fetchEvents }, dispatch)
+  return bindActionCreators({ fetchEvents, clearEvents }, dispatch)
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventMap)
--- src/reducers/reducer_events.js
+++ src/reducers/reducer_events.js
@@ -2,6 +2,8 @@ const EventsReducer = (state = [] , action) => {
   switch (action.type) {
     case 'SET_EVENTS':
       return action.payload
+    case 'CLEAR_EVENTS':
+      return []
     default:
       return state
   }
