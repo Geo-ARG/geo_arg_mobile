d4bdf410543723b717e43a23dac490f926acb126
Fadly Kayo <fadly.kayo@googlemail.com>
2017-03-21 15:02:00 +0700

    WIP 6

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,43 +1,44 @@
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  View,
-  Image,
-  Text,
-  BackAndroid
-} from 'react-native';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body } from 'native-base';
+import { StyleSheet, View, Image, Text, BackAndroid, Dimensions } from 'react-native';
+import { Container, Header, Title, Content, Button, Left, Right, Body } from 'native-base';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-
+const {height, width} = Dimensions.get('window');
 
 export default class Home extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      size: {width, height: height * 0.85}
+    }
+  }
+
   render() {
     BackAndroid.addEventListener('LockBack', function() {
       return true;
     })
     return (
       <Container>
-        <Header>
+        <Header style={{height: height * 0.1}}>
           <Left>
             <Image
-              style={styles.badge2}
+              style={styles.headerIcon}
               source={require('../assets/logo.png')}
-              />
+            />
           </Left>
           <Body>
-            <Title>ARG Home</Title>
+            <Title>Home</Title>
           </Body>
           <Right />
         </Header>
-        <Content>
+        <Content style={{height: height * 0.9, backgroundColor: 'grey'}}>
           <View style={styles.messageBox}>
             <Image
               style={styles.badge}
               source={require('../assets/logo.png')}
-              />
+            />
           </View>
           <Button
-            block warning style={{ marginBottom: 10, marginTop: 10, marginLeft: 10, marginRight: 10}}
+            block warning style={{borderRadius:10, marginBottom: 10, marginLeft: 10, marginRight: 10}}
             onPress={() => this.props.navigator.push({
               page: 'event'
             })}
@@ -46,7 +47,7 @@ export default class Home extends Component {
             <Icon name="event" />
           </Button>
           <Button
-            block warning style={{ marginBottom: 10, marginLeft: 10, marginRight: 10}}
+            block warning style={{borderRadius:10, marginBottom: 10, marginLeft: 10, marginRight: 10}}
             onPress={() => this.props.navigator.push({
               page: 'map'
             })}
@@ -55,7 +56,7 @@ export default class Home extends Component {
             <Icon name="add-location" />
           </Button>
           <Button
-            block warning style={{ marginBottom: 10, marginLeft: 10, marginRight: 10}}
+            block warning style={{borderRadius:10, marginBottom: 10, marginLeft: 10, marginRight: 10}}
             onPress={() => this.props.navigator.push({
               page: 'profile'
             })}
@@ -63,8 +64,8 @@ export default class Home extends Component {
             <Text>My Profile</Text>
             <Icon name="account-circle" />
           </Button>
-          <Button 
-            block warning style={{marginLeft: 10, marginRight: 10}}
+          <Button
+            block warning style={{borderRadius:10, marginBottom: 10, marginLeft: 10, marginRight: 10}}
             onPress={() => this.props.navigator.push({
               page: 'help'
             })}
@@ -95,9 +96,10 @@ var styles = StyleSheet.create({
     height: 169,
     width: 151,
   },
-  badge2: {
+  headerIcon: {
     alignSelf: 'center',
-    height: 40,
-    width: 30,
+    height: 60,
+    width: 60,
+    marginTop: 5
   }
 });
