24ba81597f6b69f3633909f480e6e2ff768b3706
Syanmil <syanmil@yahoo.com>
2017-03-24 05:37:06 +0700

    updating

--- index.android.test.js
+++ index.android.test.js
@@ -4,7 +4,7 @@ import IndexAndroid from './index.android'
 import Camera from 'react-native-camera';
 jest.mock('react-native-camera', () => 'Camera');
 
-test('test', () => {
+it('renders correctly', () => {
   const tree = renderer.create(
     <IndexAndroid />
   ).toJSON();
