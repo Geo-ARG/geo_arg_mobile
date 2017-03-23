c263d4963b88fb80f8bd4f968e05b91bb021ef2e
Syanmil <syanmil@yahoo.com>
2017-03-23 08:33:33 +0700

    add alert on completion

--- src/components/GameEvent.js
+++ src/components/GameEvent.js
@@ -85,6 +85,19 @@ class GameEvent extends React.Component {
     );
   }
 
+  componentWillReceiveProps(nextprops){
+    if(nextprops.progressCircle === 1){
+      Alert.alert(
+        'Alert Title',
+        'My Alert Msg',
+        [{text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
+        {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+        {text: 'OK', onPress: () => console.log('OK Pressed')}], 
+        { cancelable: false }
+      )
+    }
+  }
+
   componentWillUnmount(){
     navigator.geolocation.clearWatch(this.watchId);
   }
